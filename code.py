bd3c692de4f71cb0508a8ca722071dad370088cf
Joseph Huttner <jhuttner@appnexus.com>
2014-12-09 16:44:10 +0000

    Update comment block (1) on master.

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 # This is a Python comment block that is in the master branch only.
 # I am going to continue updating this comment block
 # with dates on master only.
+# Another comment. (Added later)
 
 def foo():
     return 'bar'
